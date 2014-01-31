c037b818997010249b3ca8fa044515e21abc4d55
danying <danyinghu@yahoo.de>
2014-01-30 17:19:02 -0800

    DH:30.01

--- src/raven/utils.cpp
+++ src/raven/utils.cpp
@@ -17,12 +17,13 @@
  * along with Raven 2 Control.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/**
- * utils.h - some common utility functions
+/** 
+ * \file utils.cpp
+ * \brief some common utility functions
  *      toShort - convert an int to a short int
  *
- * Kenneth Fodero
- * Biorobotics Lab 2005
+ * \author Kenneth Fodero, Biorobotics Lab 
+ * \date 2005
  *
  */
 
@@ -32,12 +33,11 @@
 #include "defines.h"
 
 extern int NUM_MECH;
-/*
- * toShort - function that takes an integer and places it in the
- *   target short int, reporting under/overflow
- *
- * inputs - value
- *
+/**\fn int toShort(int value, short int *target)
+ * \brief function that takes an integer and places it in the target short int, reporting under/overflow
+ * \param value int value to convert
+ * \param target short int pointer that points to the converted short value
+ * \return 1 if overflow; -1 if underflow; 0 if no problem
  */
 int toShort(int value, short int *target)
 {
@@ -63,20 +63,26 @@ int toShort(int value, short int *target)
 }
 
 
-/**
-*    Iterate over all joints of all mechanisms.
-*    To start iteration, call function with _joint == _mech == NULL
-*    Function iterates by incrementing jnum and mnum from zero and returning the appropriate mech and joint.
-*    Iteration terminates when function is called with jnum == MAX_DOF-1 and mnum = NUM_MECH-1.
-*       In the terminating case, mnum and jnum are not modified.  _joint and _mech are also unmodified in that case.
-*
-*    Precondition:  mechnum and jnum point to _previous_ joint.
-*                   At start of iteration _mech == _joint == NULL
-*
-*    Postcondition: jnum and mnum are incremented or reset as necessary
-*                   _mech points to device0.mech[mechnum]
-*                   _joint points to device0.mech[mechnum].joint[jnum+1]
-*/
+/**\fn int loop_over_joints(struct robot_device* device0, struct mechanism*& _mech, struct DOF*& _joint, int& mechnum, int& jnum)
+ * \brief Iterate over all joints of all mechanisms
+ *  To start iteration, call function with _joint == _mech == NULL
+ *  Function iterates by incrementing jnum and mnum from zero and returning the appropriate mech and joint.
+ *  Iteration terminates when function is called with jnum == MAX_DOF-1 and mnum = NUM_MECH-1.
+ *  In the terminating case, mnum and jnum are not modified.  _joint and _mech are also unmodified in that case.
+ *
+ *  Precondition:  mechnum and jnum point to _previous_ joint.
+ *                   At start of iteration _mech == _joint == NULL
+ *
+ *  Postcondition: jnum and mnum are incremented or reset as necessary
+ *                   _mech points to device0.mech[mechnum]
+ *                   _joint points to device0.mech[mechnum].joint[jnum+1]
+ * \param device0 a pointer points to the robot_device struct
+ * \param _mech   a pointer points to the mechanism struct, and it is updated at the end of the function
+ * \param _joint  a pointer points to the DOF struct, and it is updated at the end of the function
+ * \param mechnum integer value represents the mechanism id, and it is updated at the end of the function
+ * \param jnum    integer value represents the joint id, and it is updated at the end of the function
+ * \return 0 if reaches to last joint index; 1 elsewise
+ */
 int loop_over_joints(struct robot_device* device0, struct mechanism*& _mech, struct DOF*& _joint, int& mechnum, int& jnum)
 {
     // Initialize iterators
@@ -111,19 +117,23 @@ int loop_over_joints(struct robot_device* device0, struct mechanism*& _mech, str
     return 1;
 }
 
-/**
-*    Iterate over all joints of one mechanism.
-*    To start iteration, call function with _joint == NULL
-*    Function iterates by incrementing jnum from zero and returning the appropriate joint.
-*    Iteration terminates when function is called with jnum == MAX_DOF-1.
-*       In the terminating case, jnum is not modified.  _joint is also unmodified in that case.
-*
-*    Precondition:  jnum point to _previous_ joint.
-*                   At start of iteration _joint == NULL
-*
-*    Postcondition: jnum is incremented or reset as necessary
-*                   _joint points to mech->joint[jnum+1]
-*/
+/**\fn loop_over_joints(struct mechanism* _mech, struct DOF*& _joint, int& jnum)
+ * \brief Iterate over all joints of one mechanism.
+ *  To start iteration, call function with _joint == NULL
+ *  Function iterates by incrementing jnum from zero and returning the appropriate joint.
+ *  Iteration terminates when function is called with jnum == MAX_DOF-1.
+ *  In the terminating case, jnum is not modified.  _joint is also unmodified in that case.
+ *
+ *  Precondition:  jnum point to _previous_ joint.
+ *                   At start of iteration _joint == NULL
+ *
+ *  Postcondition: jnum is incremented or reset as necessary
+ *                   _joint points to mech->joint[jnum+1]
+ * \param _mech   a pointer points to the mechanism struct, and it is updated at the end of the function
+ * \param _joint  a pointer points to the DOF struct, and it is updated at the end of the function
+ * \param jnum    integer value represents the joint id, and it is updated at the end of the function
+ * \return 0 if reaches to last joint index; 1 elsewise
+ */
 int loop_over_joints(struct mechanism* _mech, struct DOF*& _joint, int& jnum)
 {
     // Initialize iterators
@@ -150,10 +160,20 @@ int loop_over_joints(struct mechanism* _mech, struct DOF*& _joint, int& jnum)
 }
 
 
+/**\fn int is_toolDOF(struct DOF *_joint)
+ * \brief check if the current joint is a toolDOF
+ * \param _joint a DOF struct 
+ * \return 1 if the joint is a toolDOF;elsewise 0
+ */
 int is_toolDOF(struct DOF *_joint){
     return is_toolDOF(_joint->type);
 }
 
+/**\fn int is_toolDOF(int jointType)
+ * \brief check if the current joint is a toolDOF
+ * \param _joint an integer value of joint type
+ * \return 1 if the joint is a toolDOF;elsewise 0
+ */
 int is_toolDOF(int jointType)
 {
     if ( jointType == TOOL_ROT_GOLD || jointType == TOOL_ROT_GREEN)
@@ -167,6 +187,13 @@ int is_toolDOF(int jointType)
 
     return 0;
 }
+
+
+/**\fn int tools_ready(struct mechanism *mech)
+ * \brief check if all tool is ready of the current mechanism during homing precedure
+ * \param mech a pointer points to the mechanism struct
+ * \return 1 if and only if all toolDOFs are ready, elsewise 0
+ */
 int tools_ready(struct mechanism *mech)
 {
     if ( mech->joint[TOOL_ROT].state != jstate_ready )
@@ -182,6 +209,12 @@ int tools_ready(struct mechanism *mech)
 }
 
 
+
+/**\fn int robot_ready(struct robot_device* device0)
+ * \brief check if robot is ready during homing procedure
+ * \param mech a pointer points to the robot_device struct
+ * \return 1 if and only if all DOFS are ready, elsewise 0
+ */
 int robot_ready(struct robot_device* device0)
 {
     struct mechanism* _mech = NULL;
@@ -197,14 +230,13 @@ int robot_ready(struct robot_device* device0)
 }
 
 
-/*
- * strtok - function to tokenize a string
+/**\fn void strtoken(char *str, char *result, char delim)
+ * \brief function to tokenize a string
+ * \param str - the string
+ * \param result - the resulting string
+ * \param delim - the delimeter
  *
- * inputs - str - the string
- *          result - the resulting string
- *          delim - the delimeter
- *
- * returns location of delimeter
+ * \return void
  */
 void strtoken(char *str, char *result, char delim)
 {
@@ -238,6 +270,13 @@ void strtoken(char *str, char *result, char delim)
     return;
 }
 
+
+/**\fn void strcopy(const char *src, char *dest)
+ * \brief function to copy a string from source to destionation
+ * \param src - the source string
+ * \param dest - the resulting string
+ * \return void
+ */
 void strcopy(const char *src, char *dest)
 {
     int i = 0;
@@ -251,8 +290,13 @@ void strcopy(const char *src, char *dest)
     dest[i] = NULL;
 }
 
-/**
- * tsSubtract() - returns time1-time2  or  (0,0) if time2>time1
+
+
+/**\fn struct  timespec  tsSubtract ( struct  timespec  time1, struct  timespec  time2)
+ * \brief function to get time interval
+ * \param time1 - struct  timespec
+ * \param time2 - struct  timespec
+ * \return time1-time2  or  (0,0) if time2>time1
  */
 struct  timespec  tsSubtract ( struct  timespec  time1,
                                            struct  timespec  time2)
@@ -282,6 +326,14 @@ struct  timespec  tsSubtract ( struct  timespec  time1,
     return (result);
 }
 
+
+
+/**\fn void getQuaternion(float* Q, float mat[3][3])
+ * \brief function to convert a rotation matrix to quanternion
+ * \param Q - a float pointer
+ * \param mat - a float 3x3 multidimension array
+ * \return void
+ */
 void getQuaternion(float* Q, float mat[3][3])
 {
     Q[_Qw] = sqrt( fmax( 0, 1 + mat[0][0] + mat[1][1] + mat[2][2] ) ) / 2;
@@ -294,6 +346,12 @@ void getQuaternion(float* Q, float mat[3][3])
     Q[_Qz] = copysignf( Q[3], mat[1][0] - mat[0][1] );
 }
 
+
+/**\fn void set_posd_to_pos(struct robot_device* device0)
+ * \brief set the current mechanism end pos to the designed pose
+ * \param device0 a pointer points to the robot_device struct
+ * \return void
+ */
 void set_posd_to_pos(struct robot_device* device0)
 {
     for (int m = 0; m < NUM_MECH; m++) {
