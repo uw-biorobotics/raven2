1577a3380e05c3d8929a5db206a6655479af5823
Blake Hannaford <blake.hannaford@gmail.com>
2014-03-21 11:24:12 -0700

    Update doxygengroups.h

--- Doxygen/specialFiles/doxygengroups.h
+++ Doxygen/specialFiles/doxygengroups.h
@@ -1,11 +1,11 @@
 /**
-*	\defgroup Control Control System related
-*	\defgroup Networking Networking related
-*	\defgroup ROS Robot Operating System related
-*	\defgroup UI User Interface
+*	\defgroup Control 
+*	\defgroup Networking
+*	\defgroup ROS
+*	\defgroup UI
 *	\defgroup DataLogging
-*	\defgroup DataStructures Key data structures
-*	\defgroup IO Physical low-level IO (usb and parallel)
-* 	\defgroup Kinematics Kinematic Computations 
+*	\defgroup DataStructures
+*	\defgroup IO IO (Physical low-level IO (usb and parallel))
+* 	\defgroup Kinematics 
 */
 
