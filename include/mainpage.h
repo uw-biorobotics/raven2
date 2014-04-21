51525bfaa632f55fd16aafd50acc9558703c21f6
Blake Hannaford <blake.hannaford@gmail.com>
2014-04-21 15:51:34 -0700

    Added a nice Main Page for the Doxygen docs.  If it doesn't show, make sure Doxygen is also pointed to the include/ directory.

---
+++ include/mainpage.h
@@ -0,0 +1,20 @@
+// mainpage.h
+//   Raven 2 Doxygen mainpage documentation
+//    (this file is not part of the source build)
+
+/** \file mainpage.h
+ *  \mainpage   Raven II source code (University of Washington BRL version)
+
+\image html RavenMask.jpg "Native American RAVEN mask"
+\image html IMG_0227whiteback_720.jpg  "RavenII Surgical Robotics Research Platform"
+
+The control software for the Raven Surgical Robotics Research Platform, developed primarily at the University of Washington, is open source.  This is the documentation.   Full source code is available on github at
+
+<https://github.com/uw-biorobotics/raven2>
+
+Eventually code from other Raven-II sites will be merged with this code-base to create a definitive Raven-II release.
+
+You may purchase the Raven II from [Applied Dexterity Inc.](http://AppliedDexterity.com)
+
+
+*/
