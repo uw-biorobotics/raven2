08764d2347b503c356387d3af142e4efb8e5a677
Blake Hannaford <blake.hannaford@gmail.com>
2014-04-21 16:24:41 -0700

    Undid markdown conversion since only supported on Doxygen > 1.8

--- include/mainpage.h
+++ include/mainpage.h
@@ -8,7 +8,8 @@
 \image html RavenMask.jpg              "Native American RAVEN mask"
 \image html IMG_0227whiteback_720.jpg  "RavenII Surgical Robotics Research Platform"
 
-# Raven II Documentation Set
+Raven II Documentation Set
+
 
 The control software for the Raven Surgical Robotics Research Platform, developed primarily at the University of Washington, is open source.  This is the documentation.   Full source code is available on github at
 
@@ -16,7 +17,8 @@ The control software for the Raven Surgical Robotics Research Platform, develope
 
 Eventually code from other Raven-II sites will be merged with this code-base to create a definitive Raven-II release.
 
-## Contributors (roughly chronological order):
+Contributors (roughly chronological order):
+
 
   - Hawkeye King
   - Blake Hannaford
@@ -30,7 +32,8 @@ Eventually code from other Raven-II sites will be merged with this code-base to 
   - Mohammad Haghighipanah
 
 
-## Reference Material:
+Reference Material:
+
   - Raven II Wiki:  (https://brl.ee.washington.edu/ravenIIwiki/index.php/Main_Page)
   - Raven II Developer Blog: (http://r2db.tumblr.com/)
   - Kinematic Model: (https://www.ee.washington.edu/techsite/papers/refer/UWEETR-2012-0006.html)
@@ -39,7 +42,7 @@ Eventually code from other Raven-II sites will be merged with this code-base to 
   - Applied Dexterity Inc.: (http://AppliedDexterity.com)
 
 
-## Obtaining Raven-II
+Obtaining Raven-II
 
 You may purchase the Raven II from [Applied Dexterity Inc.](http://AppliedDexterity.com)
 */
