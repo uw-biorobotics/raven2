535f5dd74095a955d838d00672a3304c86821ce5
Melody <melodysu83@gmail.com>
2016-08-25 14:25:38 -0700

    Update fwd_kinematics.cpp
    add some comments to the file.

---
+++ src/raven/fwd_kinematics.cpp
@@ -0,0 +1,35 @@
+/* Raven 2 Control - Control software for the Raven II robot
+ * Copyright (C) 2005-2012  H. Hawkeye King, Blake Hannaford, and the University of Washington BioRobotics Laboratory
+ *
+ * This file is part of Raven 2 Control.
+ *
+ * Raven 2 Control is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Raven 2 Control is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Raven 2 Control.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+*   \file fwd_kinematics.cpp
+*
+*        \brief This is an empty file.
+*
+*        \fn These is no function in fwd_kinematics.cpp file. 
+*            Functions marked with "*" are called explicitly from other files.
+*              (1) fwdKin                   :This function is declared in fwd_kinematics.h file but never defined here.
+*              (2) fwdMechKin               :This function is declared in fwd_kinematics.h file but never defined here.
+*
+*       \author Hawkeye King
+*
+*       \date ??
+*
+*       \ingroup Control
+*/
