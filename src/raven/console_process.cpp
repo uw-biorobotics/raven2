ebb81b0db9ba1994a5c7d47808f1bcde3715916d
Melody <melodysu83@gmail.com>
2016-08-25 13:45:03 -0700

    Update console_process.cpp
    add some comments to the file.

--- src/raven/console_process.cpp
+++ src/raven/console_process.cpp
@@ -25,6 +25,12 @@
 *
 *  	\author Hawkeye King
 *
+* 	\fn These are the 3 functions in console_process.cpp file. 
+*           Functions marked with "*" are called explicitly from other files.
+* 	       *(1) getdir		 	:uses (2)(3)
+*       	(2) get_board_id_from_filename	 
+* 		(3) write_zeros to board	
+*
 *  	\date ??
 *
 *  	\ingroup IO
